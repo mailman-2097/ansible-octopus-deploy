2b72723ad50e4023dc8db102c0ba071a1055f6ba
Nishad Saithaly <13964689+mailman-2097@users.noreply.github.com>
2022-07-20 17:22:06 +1000

    first commit signed

---
+++ README.md
@@ -0,0 +1,3 @@
+# vagrant-ubuntu22
+Sample Vagrant Box for Ubuntu 2204
+# ansible-octopus-deploy
---
+++ script/setup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash -e
+cp /etc/apt/sources.list /etc/apt/sources.list.0
+sed -i -E "s|http://us.|http://|g" /etc/apt/sources.list
+apt-get update -y
+apt-get upgrade -y
+apt-get install software-properties-common
+add-apt-repository --yes --update ppa:ansible/ansible
+apt-get install ansible -y
+ansible-galaxy collection install community.general
+pushd /vagrant
+ansible-playbook provisioning/playbook.yml
+popd
